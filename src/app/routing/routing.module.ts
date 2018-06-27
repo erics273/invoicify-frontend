a7d667c84db9ddd673c1219e1598d8af5cfc511a
graypeck <Graydon.Peck@Ally.com>
2018-06-27 14:15:49 -0400

    Navigation bewteen pages working.

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent }   from '../company/company.component';
 import { CompanyFormComponent }   from '../company-form/company-form.component';
 import { HomeComponent }   from '../home/home.component';
+import { BillingRecordComponent } from '../billing-record/billing-record.component';
+import { InvoicesComponent } from '../invoices/invoices.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent },
   { path: 'company',  component: CompanyComponent },
+  { path: 'billing-record',  component: BillingRecordComponent },
   { path: 'company/edit/:id', component: CompanyFormComponent },
   { path: 'company/add', component: CompanyFormComponent },
+  { path: 'invoices', component: InvoicesComponent},
 ];
 
 @NgModule({
